0feb8245304f47b3edd800166c66174abba8f3e9
Vasileios Raptis <73752606+basilisraptis@users.noreply.github.com>
2021-04-27 20:19:32 +0300

    Update sc_pm10.js

--- vasilisScript/sc_pm10.js
+++ vasilisScript/sc_pm10.js
@@ -12,7 +12,7 @@ var pm10_group = new L.featureGroup([]);
 
         map.createPane('pane_sc_pm10_3_1');
         map.getPane('pane_sc_pm10_3_1').style.zIndex = 411;
-        var img_sc_pm10_3_1 = 'dataa/sc_pm10_3_1.png';
+        var img_sc_pm10_3_1 = 'data/sc_pm10_3_1.png';
         var img_bounds_sc_pm10_3_1 = [[55.20040684898399,-6.845635567370293],[57.84226226691833,-1.5880110819219218]];
         var layer_sc_pm10_3_1 = new L.imageOverlay(img_sc_pm10_3_1,
                                             img_bounds_sc_pm10_3_1,
@@ -23,7 +23,7 @@ var pm10_group = new L.featureGroup([]);
 
         map.createPane('pane_sc_pm10_2_2');
         map.getPane('pane_sc_pm10_2_2').style.zIndex = 412;
-        var img_sc_pm10_2_2 = 'dataa/sc_pm10_2_2.png';
+        var img_sc_pm10_2_2 = 'data/sc_pm10_2_2.png';
         var img_bounds_sc_pm10_2_2 = [[56.13246910672815,-8.13395819073731],[58.70549992259696,-2.7371037523882906]];
         var layer_sc_pm10_2_2 = new L.imageOverlay(img_sc_pm10_2_2,
                                             img_bounds_sc_pm10_2_2,
@@ -34,7 +34,7 @@ var pm10_group = new L.featureGroup([]);
 
         map.createPane('pane_sc_pm10_1_3');
         map.getPane('pane_sc_pm10_1_3').style.zIndex = 413;
-        var img_sc_pm10_1_3 = 'dataa/sc_pm10_1_3.png';
+        var img_sc_pm10_1_3 = 'data/sc_pm10_1_3.png';
         var img_bounds_sc_pm10_1_3 = [[58.64359600218226,-3.5636723146828193],[60.966919088404346,-0.4625877775025636]];
         var layer_sc_pm10_1_3 = new L.imageOverlay(img_sc_pm10_1_3,
                                             img_bounds_sc_pm10_1_3,
